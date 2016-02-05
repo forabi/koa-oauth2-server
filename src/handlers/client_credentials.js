43e48bb874d91e792798a52d41d929420f9b6a50
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-02-05 21:30:50 +0200

    Client credentials passes all tests

--- src/handlers/client_credentials.js
+++ src/handlers/client_credentials.js
@@ -1,13 +1,13 @@
-import { InvalidInputError } from '../errors';
+import { InvalidInputError, toOAuthError } from '../errors';
 
 export function handleTokenRequest({
-  createAccessToken, isClientSecretValid,
+  createAccessToken, areClientCredentialsValid,
   isRequestedScopeValid,
 }) {
   return async (ctx, next) => {
     const {
       grant_type, client_id, client_secret,
-      scope, ttl, state,
+      scope,
     } = ctx.request.body;
     if (grant_type !== 'client_credentials') {
       return next();
@@ -18,13 +18,20 @@ export function handleTokenRequest({
     if (!client_secret || !String(client_secret).length) {
       throw new InvalidInputError('client_secret');
     }
-    if (await isClientSecretValid({ client_id, client_secret }) === false) {
+    if (await areClientCredentialsValid({ client_id, client_secret }) === false) {
       throw new InvalidInputError('client_secret');
     }
-    const token = await createAccessToken({
-      client_id, scope, state, ttl,
-      createRefreshToken: true,
-    });
-    ctx.body = token;
+    if (scope && await isRequestedScopeValid({ client_id, scope }) === false) {
+      throw new InvalidInputError('scope');
+    }
+    try {
+      const token = await createAccessToken({
+        client_id, scope,
+        createRefreshToken: false,
+      });
+      ctx.body = token;
+    } catch (e) {
+      ctx.body = toOAuthError(e).toJSON();
+    }
   };
 }
--- src/test/client_credentials.spec.js
+++ src/test/client_credentials.spec.js
@@ -7,6 +7,7 @@ import uniqueId from 'lodash.uniqueid';
 describe('Client Credentials Grant Type', () => {
   describe('Token Request handler', () => {
     beforeEach(function setUpTest() {
+      this.token = { token: uniqueId('token_') };
       this.ctx = {
         request: {
           body: {
@@ -19,9 +20,9 @@ describe('Client Credentials Grant Type', () => {
         redirect: createSpy(),
       };
       this.fns = {
-        createAccessToken: createSpy(),
-        isClientSecretValid: createSpy(),
-        isRequestedScopeValid: createSpy(),
+        createAccessToken: createSpy().andReturn(this.token),
+        areClientCredentialsValid: createSpy().andReturn(true),
+        isRequestedScopeValid: createSpy().andReturn(true),
       };
       this.next = createSpy();
     });
@@ -51,10 +52,42 @@ describe('Client Credentials Grant Type', () => {
       expect(fns.createAccessToken).toHaveBeenCalled();
       expect(ctx.body).toBe(token);
     });
-    it('should reject on invalid "client_id"');
-    it('should reject on invalid "client_secret"');
-    it('should reject on invalid "scope"');
-    it('should set a server error as response body');
+
+    describe('Invalid parameters', () => {
+      it('should reject on invalid "client_id" and/or "client_secret"', function () {
+        this.fns.areClientCredentialsValid = createSpy().andReturn(false);
+        this.errorHandler = createSpy().andCall(e => {
+          expect(e.message)
+          .toMatch(/client_id|client_secret/)
+          .toMatch(/missing|invalid input/i);
+        });
+      });
+
+      it('should reject on invalid "scope"', function () {
+        this.fns.isRequestedScopeValid = createSpy().andReturn(false);
+        this.errorHandler = createSpy().andCall(e => {
+          expect(e.message)
+          .toMatch(/scope/)
+          .toMatch(/missing|invalid input/i);
+        });
+      });
+
+      afterEach(async function() {
+        const { fns, ctx, next, errorHandler } = this;
+        await handleTokenRequest(fns)(ctx, next).catch(errorHandler);
+        expect(errorHandler).toHaveBeenCalled();
+      });
+    });
+
+    it('should set a server error as response body', async function() {
+      const { ctx, next, fns } = this;
+      const errorMessage = uniqueId('Error_');
+      fns.createAccessToken = createSpy().andThrow(new Error(errorMessage));
+      await handleTokenRequest(fns)(ctx, next);
+      expect(next).toNotHaveBeenCalled();
+      expect(ctx.body.error).toExist();
+      expect(ctx.body.error_description).toContain(errorMessage);
+    });
     describe('On valid requests', () => {
       beforeEach(async function setUpTest() {
         const { ctx, next, fns } = this;
@@ -62,11 +95,31 @@ describe('Client Credentials Grant Type', () => {
         expect(next).toNotHaveBeenCalled();
         expect(ctx.redirect).toNotHaveBeenCalled();
       });
-      it('if scope is included, it should be validated');
-      it('if scope is valid, it should be passed to token generation function');
-      it('should validate client credentials');
-      it('should not redirect, instead sets the token as response body');
-      it('response should not include a refresh token');
+      it('if scope is included, it should be validated', function () {
+        const { ctx, fns } = this;
+        const { client_id, scope } = ctx.request.body;
+        expect(fns.isRequestedScopeValid)
+          .toHaveBeenCalledWith({ client_id, scope });
+      });
+      it('token generation function should be called with all parameters', function () {
+        const { ctx, fns } = this;
+        const { client_id, scope } = ctx.request.body;
+        expect(fns.createAccessToken)
+          .toHaveBeenCalledWith({ client_id, scope, createRefreshToken: false });
+      });
+      it('should validate client credentials', function () {
+        const { ctx, fns } = this;
+        const { client_id, client_secret } = ctx.request.body;
+        expect(fns.areClientCredentialsValid)
+          .toHaveBeenCalledWith({ client_id, client_secret });
+      });
+      it('should not redirect, instead sets the token as response body', function () {
+        expect(this.ctx.redirect).toNotHaveBeenCalled();
+        expect(this.ctx.body).toEqual(this.token);
+      });
+      it('response should not include a refresh token', function () {
+        expect(this.ctx.body.token.refresh_token).toNotExist();
+      });
     });
   });
 });
